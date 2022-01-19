a08394239e16c229f624f359ddc919c3f5c6e3e2
min-s9709 <78842816+min-s9709@users.noreply.github.com>
2022-01-19 18:03:33 +0900

    my challenge#4

---
+++ challenge#4/index.js
@@ -0,0 +1,23 @@
+const clockTitle = document.querySelector(".js-clock");
+
+const perMinMs = 1000*60;
+const perHourMs = perMinMs*60;
+const perDayMs = perHourMs*24;
+
+
+function getClock(){
+    const futureDate = new Date('2022-12-25T00:00:00');
+    const nowDate = new Date();
+
+    const diff = futureDate.getTime()- nowDate.getTime();
+
+    const diffDay = String(Math.floor(diff / perDayMs));
+    const diffHours = String(Math.floor((diff % perDayMs) / perHourMs)).padStart(2,"0");
+    const diffMinutes = String(Math.floor((diff % perDayMs % perHourMs) / perMinMs)).padStart(2,"0");
+    const diffSeconds = String(Math.floor((diff % perDayMs % perHourMs % perMinMs) / 1000)).padStart(2,"0");
+    
+    clockTitle.innerText = `${diffDay}d ${diffHours}h ${diffMinutes}m ${diffSeconds}s`;   
+}
+
+getClock();
+setInterval(getClock,1000);
